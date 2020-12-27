5f299a47ae5f18dcdf2cf7ca099bbe4404960069
Cullen S <cullensmi@gmail.com>
2020-12-27 09:36:25 -0500

    solved algorithm

--- src/com/csmithswim/Main.java
+++ src/com/csmithswim/Main.java
@@ -1,11 +1,12 @@
 package com.csmithswim;
+
 import java.io.*;
 import java.util.*;
 import java.text.*;
 import java.math.*;
 import java.util.regex.*;
 
-public class  Main {
+public class Main {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
@@ -15,5 +16,28 @@ public class  Main {
             a[a_i] = in.nextInt();
         }
         // Write Your Code Here
+        int numberOfSwaps = 0;
+
+        for (int i = 0; i < n; i++) {
+            // Track number of elements swapped during a single array traversal
+
+            for (int j = 0; j < n - 1; j++) {
+                // Swap adjacent elements if they are in decreasing order
+                if (a[j] > a[j + 1]) {
+                    int tmp = a[j];
+                    a[j] = a[j + 1];
+                    a[j + 1] = tmp;
+                    numberOfSwaps++;
+                }
+            }
+
+            // If no elements were swapped during a traversal, array is sorted
+            if (numberOfSwaps == 0) {
+                break;
+            }
+        }
+        System.out.println("Array is sorted in "+ numberOfSwaps+" swaps.");
+        System.out.println("First Element: "+ a[0]);
+        System.out.println("Last Element: " + a[a.length-1]);
     }
 }
