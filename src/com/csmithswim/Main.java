8ee221ddf0ea8f57eb773165cb55329f7fd71ff2
Cullen S <cullensmi@gmail.com>
2020-12-27 07:55:59 -0500

    Added code snippet

--- src/com/csmithswim/Main.java
+++ src/com/csmithswim/Main.java
@@ -1,8 +1,19 @@
 package com.csmithswim;
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
 
-public class Main {
+public class  Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] a = new int[n];
+        for(int a_i=0; a_i < n; a_i++){
+            a[a_i] = in.nextInt();
+        }
+        // Write Your Code Here
     }
 }
